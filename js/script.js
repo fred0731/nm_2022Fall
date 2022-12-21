f9ca2a2d6ea5993a4bb970b47fc430865262ceee
FRED OH <o98230703@gmail.com>
2022-12-21 09:07:36 +0900

    start hosting
    2022NM final project

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+});
+
+//typing animation
+var typed = new Typed(".typing1", {
+    strings: ["CALENDAR", "DIARY", "BOARD GAME"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+var typed = new Typed(".typing2", {
+    strings: ["Introduction"],
+    typeSpeed: 100,
+    backSpeed: 30,
+    loop: true
+});
+
+var typed = new Typed(".typing3", {
+    strings: ["Features"],
+    typeSpeed: 100,
+    backSpeed: 30,
+    loop: true
+});
+
+var typed = new Typed(".typing4", {
+    strings: ["Package Lists"],
+    typeSpeed: 100,
+    backSpeed: 30,
+    loop: true
+});
+
